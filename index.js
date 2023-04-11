b86a0fd995fca09206ece600870a940a8b03bb42
Safdar Alee <112233435+AleeSafdar@users.noreply.github.com>
2023-04-11 17:59:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// Q1=Write a function that takes two numbers as an argument and returns their sum
+
+function add(val1, val2) {
+  console.log(val1 + val2)
+}
+
+add(1, 199);
+add(1, 99);
+add(9, 90);
+
+//Q2(Solved from chatGpt) = Write a function that takes a number as an argument and return true if num is even , and false if its odd
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+function isOdd(num) {
+  return num % 2 !== 0;
+}
+
+// Example usage:
+let num1 = 10;
+let num2 = 7;
+
+console.log(num1 + ' is even? ' + isEven(num1)); // Output: 10 is even? true
+console.log(num1 + ' is odd? ' + isOdd(num1)); // Output: 10 is odd? false
+console.log(num2 + ' is even? ' + isEven(num2)); // Output: 7 is even? false
+console.log(num2 + ' is odd? ' + isOdd(num2)); // Output: 7 is odd? true
+
+
